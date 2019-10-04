4a98b21fc982c5b714d1454c056bce8295fb0be3
HelloWorld183L <46497907+HelloWorld183L@users.noreply.github.com>
2019-10-04 17:21:29 +0200

    Add files via upload

---
+++ tests/MovieCRUD.Tests/Controllers/MovieControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieCRUD;
+using MovieCRUD.Controllers;
+
+namespace MovieCRUD.Tests.Controllers
+{
+    [TestClass]
+    public class MovieControllerTest
+    {
+        [TestMethod]
+        public void List_ReturnsNotNullMovieModel()
+        {
+            // Arrange
+            MovieController controller = new MovieController();
+
+            // Act
+            ViewResult result = controller.List() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Create()
+        {
+            // Arrange
+            MovieController controller = new MovieController();
+
+            // Act
+            ViewResult result = controller.Create() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Details()
+        {
+            // Arrange
+            MovieController controller = new MovieController();
+
+            /*
+            // Act
+            ViewResult result = controller.Details() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            */
+        }
+    }
+}
