80c97783440b3503e6c54c262376b7535d7c0c2b
HelloWorld183L <46497907+HelloWorld183L@users.noreply.github.com>
2019-11-29 15:11:15 +0000

    Created email service class

---
+++ src/MovieCRUD/TwoFactorServices/EmailService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity;
+using System.Threading.Tasks;
+
+namespace MovieCRUD.TwoFactorServices
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // TODO: Create email service
+            return Task.FromResult(0);
+        }
+    }
+}
