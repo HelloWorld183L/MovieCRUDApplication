1c2a11ce4352c3a3c1c8fdad4540477713197806
HelloWorld183L <46497907+HelloWorld183L@users.noreply.github.com>
2019-11-29 15:20:51 +0000

    Add files via upload

---
+++ src/MovieCRUD.Data/Entities/Movie.cs
@@ -0,0 +1,16 @@
+﻿using MovieCRUD.Data.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieCRUD.Data
+{
+    public class Movie : IEntity
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public Rating Rating { get; set; }
+        public int Id { get; set; }
+    }
+}
