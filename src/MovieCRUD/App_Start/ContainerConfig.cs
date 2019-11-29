81e2d92507317b65859ae6a70e9ad6a8a064ea28
HelloWorld183L <46497907+HelloWorld183L@users.noreply.github.com>
2019-11-29 15:14:58 +0000

    Add files via upload

---
+++ src/MovieCRUD/App_Start/ContainerConfig.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using MovieCRUD.Data;
+using MovieCRUD.Data.Services;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace MovieCRUD.App_Start
+{
+    public class ContainerConfig
+    {
+        public static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetAssembly(typeof(MvcApplication)));
+
+            builder.RegisterType<Repository<Movie>>()
+                .AsImplementedInterfaces()
+                .InstancePerRequest();
+            builder.RegisterType<ApplicationDbContext>()
+                .AsSelf()
+                .InstancePerRequest();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
