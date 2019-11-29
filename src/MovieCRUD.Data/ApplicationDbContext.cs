e4590ffbb5ac07f0e4354c0495381a687909f394
HelloWorld183L <46497907+HelloWorld183L@users.noreply.github.com>
2019-11-29 15:21:46 +0000

    Add files via upload

---
+++ src/MovieCRUD.Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MovieCRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public ApplicationDbContext() : base("ApplicationDb")
+        {
+            var ensureDllIsCopied = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+        }
+    }
+}
